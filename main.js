2640dd0a7cb00f410439d13968482283e01d3cf5
Francine <Francine_landim@hotmail.com>
2023-01-22 18:31:24 -0300

    2 correção da tarefa 7

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const form = document.getElementById('form-validacao');
-const valorA = document.getElementById('Campo A');
-const valorB = document.getElementById('Campo B');
+const form = document.getElementById("form-validacao");
 let formEValido = false;
 
-function maiorMenor (fieldA, fieldB) {
+function maiorMenor(fieldA, fieldB) {
   return fieldB > fieldA;
 }
 
-form.addEventListener('submit', function (event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const mensagemSucesso = `O valor do Campo B <b>${valorB.value}</b> é maior que o valor do Campo A <b>${valorA.value}</b>.`;
-  const containerMensagemSucesso = document.querySelector('.success-message');
-  const containerMensagemErro = document.querySelector('.error-message');
-  formEValido = maiorMenor (valorA.value, valorB.value);
+  const valorA = parseInt(document.getElementById("Campo A").value);
+  const valorB = parseInt(document.getElementById("Campo B").value);
+  const mensagemSucesso = `O valor do Campo B <b>${valorB}</b> é maior que o valor do Campo A <b>${valorA}</b>.`;
+  const containerMensagemSucesso = document.querySelector(".success-message");
+  const containerMensagemErro = document.querySelector(".error-message");
+  formEValido = maiorMenor(valorA, valorB);
   if (formEValido) {
     containerMensagemSucesso.innerHTML = mensagemSucesso;
-    containerMensagemSucesso.style.display = 'block';
-    containerMensagemErro.style.display = 'none';
-    valorA.value = '';
-    valorB.value = '';
+    containerMensagemSucesso.style.display = "block";
+    containerMensagemErro.style.display = "none";
+    /*valorA = '';
+    valorB = '';*/
   } else {
-    containerMensagemErro.style.display = 'block';
-    containerMensagemSucesso.style.display = 'none';
+    containerMensagemErro.style.display = "block";
+    containerMensagemSucesso.style.display = "none";
   }
 });
